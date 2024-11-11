98580795d4a36e48879c0601de5f89960ad5bfe7
ningyo-sitizen <jowymone26@gmail.com>
2024-11-11 09:19:34 +0700

    tambah tombol kembali dalam out put

--- app/src/main/java/com/example/tutorial/bangun_datar_output.java
+++ app/src/main/java/com/example/tutorial/bangun_datar_output.java
@@ -2,6 +2,8 @@ package com.example.tutorial;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -12,6 +14,7 @@ public class bangun_datar_output extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bangun_datar_output);
+        Button btn = findViewById(R.id.btnkembali);
         TextView keliling_hasil = findViewById(R.id.vHasilKeliling);
         TextView luas_hasil = findViewById(R.id.vHasilLuas);
         Intent intent = getIntent();
@@ -20,5 +23,16 @@ public class bangun_datar_output extends AppCompatActivity {
 
         keliling_hasil.setText(keliling);
         luas_hasil.setText(luas);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(bangun_datar_output.this, bangundatar_input.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
